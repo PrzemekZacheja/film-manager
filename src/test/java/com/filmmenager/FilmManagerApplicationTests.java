57c6e25999d52d1e07b818574fb43243795778c0
Przemysław Zacheja <przemek.zacheja@gmail.com>
2024-01-03 21:25:10 +0100

    First commit

---
+++ src/test/java/com/filmmenager/FilmManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmmenager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
