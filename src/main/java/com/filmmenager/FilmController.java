206c4bac2fd3bf9c0fd82f88eef62c466d67785d
Przemysław Zacheja <przemek.zacheja@gmail.com>
2024-01-04 13:58:13 +0100

    added @PathVariables to controller

--- src/main/java/com/filmmenager/FilmController.java
+++ src/main/java/com/filmmenager/FilmController.java
@@ -2,6 +2,7 @@ package com.filmmenager;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -9,14 +10,14 @@ import java.util.Collections;
 @RestController
 public class FilmController {
 
-    @GetMapping("/film")
-    public ResponseEntity<FilmSearchResponseDto> findFilm(String title) {
+    @GetMapping("/film/{title}")
+    public ResponseEntity<FilmSearchResponseDto> getFilmByTitle(@PathVariable String title) {
         return ResponseEntity.ok()
                              .body(new FilmSearchResponseDto());
     }
 
-    @GetMapping("/film/fav")
-    public ResponseEntity<FilmFavoriteResponseDto> setFavorite(String title) {
+    @GetMapping("/film/fav/{title}")
+    public ResponseEntity<FilmFavoriteResponseDto> setFavorite(@PathVariable String title) {
         return ResponseEntity.ok()
                              .body(new FilmFavoriteResponseDto());
     }
