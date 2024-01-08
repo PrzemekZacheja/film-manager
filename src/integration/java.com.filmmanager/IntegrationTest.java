bac52fcdc3afe88d932b6966d78782ae45db139b
Przemysław Zacheja <przemek.zacheja@gmail.com>
2024-01-08 15:17:58 +0100

    add steps in happypath.txt, created IntegrationTest.

---
+++ src/integration/java.com.filmmanager/IntegrationTest.java
@@ -0,0 +1,10 @@
+import com.filmmanager.FilmManagerApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FilmManagerApplication.class)
+public class IntegrationTest {
+
+
+}
+
+//TODO implement happy path
